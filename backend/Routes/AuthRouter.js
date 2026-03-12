7b173802042e6f2322d0ff1ab4dc6174eb2b3720
suchismita102 <suchismitaroy0001@gmail.com>
2026-03-13 01:26:32 +0530

    Delete backend directory

--- backend/Routes/AuthRouter.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const { signup, login } = require('../Controllers/AuthController');
-const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
-
-router.post('/login', loginValidation, login);
-router.post('/signup', signupValidation, signup);
-
-module.exports = router;
